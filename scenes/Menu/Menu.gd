a5090c19d31b3216f770d4bb891be8f1a5f266c0
Nicolás Valori Nesprías <valorinespriasnicolas@gmail.com>
2025-04-28 01:14:12 -0300

    Commit 7 - Godot: Conectar manualmente los botones
    En este commit conectamos manualmente las señales button_down de RedGenerator y VioletGenerator para que llamen a la función count_new_instance en el script de Menu.

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -8,6 +8,9 @@ func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+	
+	$RedGenerator.connect("button_down", Callable(self, "count_new_instance"))
+	$VioletGenerator.connect("button_down", Callable(self, "count_new_instance"))
 	pass
 
 func count_new_instance():
